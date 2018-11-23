26cdc9b4e024c627d19dafd7066609c98237c9b9
Ahsan <ahsan.qureshi8@gmail.com>
2018-11-22 23:13:47 -0500

    changed staticfiles_dirs to just be static not mainsite/static

--- AhsansDevSite/settings.py
+++ AhsansDevSite/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'mainSite/static/mainSite/media')
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'mainSite/static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 MEDIA_URL = '/media/'
